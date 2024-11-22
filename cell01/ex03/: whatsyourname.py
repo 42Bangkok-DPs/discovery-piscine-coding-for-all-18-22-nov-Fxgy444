1f93172864fed9006229f8aee1bcc4cd4a92d1d6
Fxgy444 <67030071@kmitl.ac.th>
2024-11-22 03:03:47 +0000

    : whatsyourname.py

--- cell01/ex03/: whatsyourname.py
+++ cell01/ex03/: whatsyourname.py
@@ -1,2 +1,3 @@
 first_name = input("กรุณากรอกชื่อของคุณ :")
-last_name = input("กรุณากรอก")
+last_name = input("กรุณากรอกนามสกุลของคุณ :")
+print("ชื่อเเละนามสกุลของคุณคือ : "+first_name + " " +last_name)
